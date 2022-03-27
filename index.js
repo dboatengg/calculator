141316195d039f5bc07ccc8cfd6a3152d1f78962
Boateng Dickson <89385798+dboatengg@users.noreply.github.com>
2022-03-27 08:45:11 +0000

    Worked on theme

--- index.js
+++ index.js
@@ -2,28 +2,6 @@ let screen = document.querySelector(".screen");
 let allButtons = document.querySelectorAll(".button");
 let buttonArray = Array.from(allButtons);
 
-// buttonArray.map((button) => {
-//   button.addEventListener("click", (e) => {
-//     const buttonText = e.target.innerText;
-//     if (buttonText == "reset") {
-//       screen.innerText = "";
-//     }
-//     if (buttonText == "del") {
-//       screen.innerText.slice(0, -1);
-//     }
-//     if (buttonText === "=") {
-//       try {
-//         screen.innerText = eval(screen.innerText);
-//       } catch {
-//         screen.innerText = "invalid input";
-//       }
-//     } else {
-//       screen.innerText += buttonText;
-//       console.log(screen.innerText);
-//     }
-//   });
-// });
-
 buttonArray.map((button) => {
   button.addEventListener("click", (e) => {
     switch (e.target.innerText) {
@@ -45,3 +23,45 @@ buttonArray.map((button) => {
     }
   });
 });
+
+//dark/light/auto mode
+const toggle = document.querySelectorAll("#toggle");
+const toggleArray = Array.from(toggle);
+
+// toggleArray.forEach((toggle) => {
+//   toggle.addEventListener("click", () => {
+//     if (toggle.className == "dark") {
+//       document.documentElement.setAttribute("data-theme", "dark");
+//       localStorage.setItem("data-theme", "dark");
+//       console.log(toggle);
+//     }
+//     if (toggle.className == "light") {
+//       document.documentElement.setAttribute("data-theme", "light");
+//       localStorage.setItem("data-theme", "dark");
+//       console.log(toggle);
+//     }
+//   });
+// });
+
+let theme = localStorage.getItem("data-theme");
+const changeToDark = () => {
+  document.documentElement.setAttribute("data-theme", "dark");
+  localStorage.setItem("data-theme", "dark");
+};
+const changeToLight = () => {
+  document.documentElement.setAttribute("data-theme", "light");
+  localStorage.setItem("data-theme", "light");
+};
+
+toggleArray.forEach((toggle) => {
+  toggle.addEventListener("click", () => {
+    let theme = localStorage.getItem("data-theme");
+    if (theme === "dark") {
+      changeToLight();
+    }
+    if (theme === "light") {
+      changeToDark();
+    }
+  });
+});
+// console.log(toggleArray);
