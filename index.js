2fe3014a91b4f251b66ac43afb6327fd6b920c74
Boateng Dickson <89385798+dboatengg@users.noreply.github.com>
2022-03-27 00:28:15 +0000

    fix general bug

--- index.js
+++ index.js
@@ -2,20 +2,48 @@ let screen = document.querySelector(".screen");
 let allButtons = document.querySelectorAll(".button");
 let buttonArray = Array.from(allButtons);
 
-buttonArray.forEach(function (button) {
+// buttonArray.map((button) => {
+//   button.addEventListener("click", (e) => {
+//     const buttonText = e.target.innerText;
+//     if (buttonText == "reset") {
+//       screen.innerText = "";
+//     }
+//     if (buttonText == "del") {
+//       screen.innerText.slice(0, -1);
+//     }
+//     if (buttonText === "=") {
+//       try {
+//         screen.innerText = eval(screen.innerText);
+//       } catch {
+//         screen.innerText = "invalid input";
+//       }
+//     } else {
+//       screen.innerText += buttonText;
+//       console.log(screen.innerText);
+//     }
+//   });
+// });
+
+buttonArray.map((button) => {
   button.addEventListener("click", (e) => {
-    const buttonText = e.target.innerText;
-    if (buttonText === "RESET") {
-      screen.innerText = "";
-    }
-    if (buttonText === "=") {
-      try {
-        screen.innerText = eval(screen.innerText);
-      } catch {
-        screen.innerText = "invalid input";
-      }
-    } else {
-      screen.innerText += buttonText;
+    switch (e.target.innerText) {
+      case "RESET":
+        screen.innerText = "";
+        break;
+      case "=":
+        try {
+          screen.innerText = eval(screen.innerText);
+        } catch {
+          screen.innerText = "Error";
+        }
+        break;
+      case "DEL":
+        if (screen.innerText) {
+          screen.innerText = screen.innerText.slice(0, -1);
+        }
+        break;
+      default:
+        screen.innerText += e.target.innerText;
     }
   });
 });
